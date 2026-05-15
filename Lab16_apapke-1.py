77cd6030d25960e4c0d54fb95c9f5f46d44d3f13
Mya Applesauce <myaapplesauce42@gmail.com>
2026-05-14 22:46:35 -0400

    added years. and made it save a png.

--- Lab16_apapke-1.py
+++ Lab16_apapke-1.py
@@ -8,7 +8,7 @@ reading data
 from pathlib import Path
 import matplotlib.pyplot as plt
 import csv
-import datetime
+from datetime import datetime
 
 try:
     path = Path('/home/mya_applesauce/Documents/lab16/OHUR.csv')
@@ -21,18 +21,21 @@ try:
     rates = []
 
     for row in reader:
+        date = datetime.strptime(row[0], '%Y-%m-%d')
         rate = float(row[1])
+        dates.append(date)
         rates.append(rate)
 
     plt.style.use('seaborn-v0_8')
     fig, ax = plt.subplots()
-    ax.plot(rates, color='red')
+    ax.plot(dates, rates, color='red')
 
     ax.set_title("Ohio Unemployment (by Month): 1976 - 2022", fontsize=25)
     ax.set_xlabel("Date", fontsize=16)
+    fig.autofmt_xdate()
     ax.set_ylabel("Unemployment Rate", fontsize=16)
     ax.tick_params(labelsize=16)
 
-    plt.show()
+    plt.savefig("ohio_unemployment.png")
 except Exception as err:
-    print("didn't work :[")
+    print("didn't work :[\nproblem is this: ", err)
